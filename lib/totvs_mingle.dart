7e3f1042202dbccd78c34a5936a3a72761600c62
Pedro Domingues <domingues.pedro@totvs.com.br>
2023-08-24 21:07:18 -0300

    primeira implementação do login com sucesso

--- lib/totvs_mingle.dart
+++ lib/totvs_mingle.dart
@@ -1,7 +1,39 @@
 library totvs_mingle;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MingleService {
+  String apiUrl;
+  String appId;
+
+  MingleService(this.apiUrl, this.appId);
+
+  Future<Map<String, dynamic>> login(
+      String email, String pwd, String alias) async {
+    Map<String, dynamic> body = {
+      'login': email,
+      'alias': alias,
+      'password': pwd,
+      'app_id': appId,
+      'device_id':'flutter'
+    };
+    var auth_api = '$apiUrl/api/v1/auth/app/authenticate';
+    final response = await http.post(
+      Uri.parse(auth_api),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(body),
+    );
+
+    if (response.statusCode == 200) {
+        Map<String, dynamic> responseData = json.decode(response.body);
+        return responseData;
+    } else {
+      Map<String, dynamic> responseData = json.decode(response.body);
+        return responseData;
+      // throw Exception('Falha ao enviar dados para a API');
+    }
+  }
 }
--- test/totvs_mingle_test.dart
+++ test/totvs_mingle_test.dart
@@ -1,12 +1,39 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:totvs_mingle/totvs_mingle.dart';
+import 'package:totvs_mingle/totvs_mingle.dart'; // Importe o pacote que você está testando.
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('MeuPacote', () {
+    test('Teste de login com sucesso', () async {
+      final meuPacote = MingleService('http://localhost:80/api', '64346072ee8aebcd686d6548'); // Inicialize seu pacote com as configurações necessárias.
+
+      const email = 'usuario@example.com';
+      const pwd = 'senha';
+      const alias = 'alias';
+      final response = await meuPacote.login(email, pwd, alias);
+
+      // Verifique se a resposta é um mapa com o token (ou outro resultado esperado).
+      expect(response, isA<Map<String, dynamic>>());
+      expect(response['token'], isNotEmpty); // Verifique se o token não está vazio.
+    });
+
+    // test('Teste de login com falha', () async {
+    //   final meuPacote = MeuPacote(); // Inicialize seu pacote com as configurações necessárias.
+
+    //   final email = 'usuario@example.com';
+    //   final pwd = 'senha_errada';
+    //   final alias = 'alias_errado';
+
+    //   // Use um token inválido para simular uma falha na autenticação.
+    //   meuPacote.setToken('token_invalido');
+
+    //   // Tente fazer login com informações incorretas.
+    //   try {
+    //     await meuPacote.login(email, pwd, alias);
+    //   } catch (e) {
+    //     // Verifique se uma exceção foi lançada (deve ocorrer uma falha na autenticação).
+    //     expect(e, isA<Exception>());
+    //     expect(e.toString(), 'Falha ao enviar dados para a API');
+    //   }
+    // });
   });
 }
